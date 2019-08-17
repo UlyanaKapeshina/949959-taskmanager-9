19608591d5d9b95d0cee067af7ae03832c048592
Ulyana <ulyanakapeshina@gmail.com>
2019-08-18 01:04:55 +0700

    добавлено создание всего борда с массивом карточек

--- src/components/board.js
+++ src/components/board.js
@@ -1,4 +1,30 @@
-export const getBoardTemplate = () => ` <section class="board container">
+import {
+  getBoardFiltersTemplate
+} from "./components/board-filters.js";
+import {
+  getTaskFormTemplate
+} from "./components/task-form.js";
+import {
+  getCardTemplate
+} from "./components/card.js";
+import {
+  getLoadMoreButtonTemplate
+} from "./components/button.js";
+import {
+  getTask
+} from "./data.js";
+
+// Создание массива карточек
+const getCards = (count) => {
+  return new Array(count).fill(``).map(getTask).map(getCardTemplate).join(``);
+};
+
+// Создание всего борда с карточками и сортировкой
+export const getBoard = (count) => ` <section class="board container">
+${getBoardFiltersTemplate()}
 <div class="board__tasks">
+${getTaskFormTemplate()}
+${getCards(count)}
 </div>
+${getLoadMoreButtonTemplate()}
 </section>`;
