b0873ee0b4c4209556d5dbded5868fec047b0c0f
Ulyana <ulyanakapeshina@gmail.com>
2019-08-20 01:20:33 +0700

    добален обработчик для кнопки загрузки

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-const CARD_QUANTITY = 10;
+const CARD_COUNT = 18;
+const CARD_LOAD_COUNT = 8;
 
 import {
   getMenuTemplate
@@ -13,7 +14,10 @@ import {
   getBoard
 } from "./components/board";
 import {
-  getCards
+  getCardsData
+} from "./components/board";
+import {
+  getCardsTemplate
 } from "./components/board";
 
 // функция добавления компонент в разметку
@@ -26,6 +30,19 @@ const main = document.querySelector(`.main`);
 // добавление компонент в разметку
 renderComponent(main.querySelector(`.main__control`), getMenuTemplate()); // добавление меню
 renderComponent(main, getSearchTemplate()); // добавление поиска
-renderComponent(main, getFiltersContainer(getCards(CARD_QUANTITY))); // добавление фильтров
+renderComponent(main, getFiltersContainer(getCardsData(CARD_COUNT))); // добавление фильтров
+renderComponent(main, getBoard(CARD_LOAD_COUNT)); // добавление контейнера скарточеками
+const loadMoreButton = main.querySelector(`.load-more`);
+let remainder = CARD_COUNT - CARD_LOAD_COUNT;
+const onLoadMoreButtonClick = () => {
+  while (remainder > CARD_LOAD_COUNT) {
+    renderComponent(main.querySelector(`.board__tasks`), getCardsTemplate(CARD_LOAD_COUNT));
+    remainder = remainder - CARD_LOAD_COUNT;
+  }
+  renderComponent(main.querySelector(`.board__tasks`), getCardsTemplate(remainder));
+  loadMoreButton.classList.add(`visually-hidden`);
+  loadMoreButton.removeEventListener(`click`, onLoadMoreButtonClick);
 
-renderComponent(main, getBoard(CARD_QUANTITY)); // добавление контейнера скарточеками
+
+};
+loadMoreButton.addEventListener(`click`, onLoadMoreButtonClick);
